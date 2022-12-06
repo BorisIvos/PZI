b7f9c59be0e8c4108a9eb7be8dd2df8cefd175ff
Boris <boris.ivos.b@gmail.com>
2022-12-06 13:07:14 +0100

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,151 @@
+<template>
+  
+ 
+    
+      
+
+  
+
+    <v-main>
+
+      <h2>Softver i alati:</h2>
+      <p>Visual Studio code,MySQL server,Heidi SQL,Git,Node.js</p>
+      <h2>Projektni zahtjev:</h2>
+      <p>Razviti aplikaciju za prodaju elektronske robe.Elektronska roba je razvrstana po kategorijama.Svaka kategorija ima sliku i ime koje predstavljavrstu te robe.Za svaku robu se vodi evidencija
+        o promjenama cijena i na aplikaciji se prikazuje najnovija cijena robe.Roba moze da bude povucena iz prodaje.Aplikacija ce imati mogucnost
+        isticanja pojedine robe na pocetnoj stranici.Posjetioci sajta mogu da robu pregledaju po kategorijama,mogu da vrse pretragu sve robe kao i pojedinacne robe.
+        Posjetioci ce morati biti prijavljeni na aplikaciju.Posjetioci mogu da registruju korisnicki nalog,da se prijavi sa svojim postojecim pristupnim parametrima
+        ili da zatrazi link za izdavanje nove lozinke.Bit ce dodana i mogucnost stavljanja robe u korpe.
+      </p>
+
+
+      <v-card
+    class="mx-auto"
+    max-width="434"
+    tile
+  >
+    <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://pps.whatsapp.net/v/t61.24694-24/155404746_2500070993633635_4747161680599388528_n.jpg?ccb=11-4&oh=01_AdQ80e5WpOYuADe45cD_04V4kUFTUUvEIzVwvE1GOqqSDg&oe=639C29D6"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Ivan Brkan
+              </v-list-item-title>
+               <p>Student sam 3 godine informatike.Najvise me zanimaju  frontend i baze podataka.</p>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+
+
+
+
+
+
+
+
+
+
+
+
+  <v-card
+    class="mx-auto"
+    max-width="434"
+    tile
+  >
+    <v-img
+      height="100%"
+      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
+    >
+      <v-row
+        align="end"
+        class="fill-height"
+      >
+        <v-col
+          align-self="start"
+          class="pa-0"
+          cols="12"
+        >
+          <v-avatar
+            class="profile"
+            color="grey"
+            size="164"
+            tile
+          >
+            <v-img src="https://pps.whatsapp.net/v/t61.24694-24/186530659_758643924775837_7299350392932826574_n.jpg?ccb=11-4&oh=01_AdQTRGmv2YYgYfTqWB4svyhlZNxVfuMq6EHoyrpDUU1Tgg&oe=639C41A2"></v-img>
+          </v-avatar>
+        </v-col>
+        <v-col class="py-0">
+          <v-list-item
+            color="rgba(0, 0, 0, .4)"
+            dark
+          >
+            <v-list-item-content>
+              <v-list-item-title class="text-h6">
+                Boris Ivos
+              </v-list-item-title>
+              <p>Student sam 3 godine informatike.Najvise me zanimaju  frontend i baze podataka.</p>
+            </v-list-item-content>
+          </v-list-item>
+        </v-col>
+      </v-row>
+    </v-img>
+  </v-card>
+
+
+
+ 
+    </v-main>
+  
+</template>
+
+<script>
+
+</script>
+
+<style>
+.moja{
+  margin-left: 1000px;
+  display: flex;
+  flex-direction: column;
+}
+.tvoja{
+  margin-right: 1000px;
+  margin-bottom: 15000px;
+  display: flex;
+  flex-direction: column;}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
